86769da0179a26bf0daf42e7e170c563d55e3973
Jagan <jramesh@wpi.edu>
2018-12-23 00:35:59 -0500

    Improved reporting

--- src/businessProcess/Reporting_BP.java
+++ src/businessProcess/Reporting_BP.java
@@ -3,6 +3,7 @@ package businessProcess;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.util.ArrayList;
 
@@ -50,7 +51,47 @@ public class Reporting_BP {
 		}
 		for(int i=0;i<Customer_id.size();i++)
 		{
-			System.out.println("/n The customer IDs are "+Customer_id.get(i));
+			System.out.println("\n The customer IDs are "+Customer_id.get(i));
+			searchCustomerDetails(Customer_id.get(i));
 		}
 }
+	
+	protected static void searchCustomerDetails(String c_id)
+	{
+		try { 
+		//JDBC Driver Setup
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		
+		//Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/qwerty", "root", "root");
+	  Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Test", "root", "Genesys@01");
+		
+		
+		
+		Statement stm = con.createStatement();
+		
+		//Select * from Customer WHERE Customer_id = '6897';
+		String sq = "Select * from Customer WHERE Customer_id = '"+c_id+"'";
+		 ResultSet rs = stm.executeQuery(sq);
+		 ResultSetMetaData metadata = rs.getMetaData();
+		 int columnCount = metadata.getColumnCount();  
+		 
+		 
+		
+		//Try multiple rows to get the values
+		while(rs.next()){
+	        String row = "";
+	        for (int i = 1; i <= columnCount; i++) {
+	            row += rs.getString(i) + ", ";          
+	        }
+		row.toString();
+		}
+		con.close();
+	}
+	catch(Exception e) {
+		
+		System.out.println(e);
+		
+	}
+	}
 }
